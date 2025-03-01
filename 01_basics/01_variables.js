a40f4317c1e1d015f5516b145c869e2de224e11a
Harshvardhan <harshvardhants7799@gmail.com>
2025-03-01 17:24:41 +0000

    01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "harshsalunkhe99@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountId = "Jaipur"
 let accountState;
 
 // accountId = 2 // not allowed
