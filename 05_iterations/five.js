48a759284bb5967abcbac2ea4dca9daa7511e564
Harshvardhan <harshvardhants7799@gmail.com>
2025-03-03 17:42:19 +0000

    five.js

---
+++ 05_iterations/five.js
@@ -0,0 +1,31 @@
+const coding = ["js", "ruby", "java", "python","cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+
+// } )
+
+
+
+//coding.forEach(printMe()) 
+
+coding.forEach ( (item, index ,arr)=> {
+    console.log(item, index, arr);
+} )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
